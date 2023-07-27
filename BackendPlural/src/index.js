0a0f3fafdccf585ed71279da8c095ccb75964616
marvisa22 <107163520+marvisa22@users.noreply.github.com>
2023-07-26 22:11:53 -0500

    Se agrego el contenido al proyecto

---
+++ BackendPlural/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import paymentRoutes from "./routes/payment.routes.js";
+import {PORT} from './config.js';
+import path from 'path'
+
+const app = express();
+
+app.use(paymentRoutes);
+
+app.use(express.static(path.resolve('src/public')));
+
+app.listen(PORT);
+console.log('Server on port', PORT);
